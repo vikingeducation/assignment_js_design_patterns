fe2d7310bf69c6ac7728ca88b1c7558ad6f6a402
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-08-27 12:28:19 -0400

    getGridSize created

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
+$(document).ready(function(){
+  controller.init();
+})
+
 //Controller
   //window prompt for grid size
   //Dont allow odd numbers
   //each grid different picture - repeat once for each pic
-  //tells view when to cover up 
+  //tells view when to cover up
   //when click tell view to render text/picture
   //GAME LOGIC
     //flips over first card
-      //if another card is already exposed 
+      //if another card is already exposed
         //check if it matches
-        //if matches 
+        //if matches
           //keep both exposed
           //green border
           //add to score
@@ -20,6 +24,21 @@
         //GAME ENDS
 
       //
+
+var controller = {
+
+  init: function() {
+    this.getGridSize();
+  },
+
+
+  getGridSize: function() {
+
+    model.setGridSize(prompt("Enter grid size: "));
+
+  }
+
+};
 
 //Model
   //will hold number for grid size
@@ -33,10 +52,28 @@
 var model = {
 
   gridSize: 0,
-  cards: ["1", "2", "3", "4", "5", "6", "7", "8"]
+  cards: ["1", "2", "3", "4", "5", "6", "7", "8"],
+
+  setGridSize: function(input) {
+    n = parseInt(input);
+    if (!isNaN(n)) {
+      if (n === 2 || n === 4) {
+        this.gridSize = n;
+      }
+      else {
+        alert("Grid size must be 2 or 4");
+        controller.getGridSize();
+      }
+    }
+    else {
+      alert("Must enter number");
+      controller.getGridSize();
+    }
+  }
+
 };
 
 //View
-  //model passes grid size for rendering  
+  //model passes grid size for rendering
   //will render the cover up
   //will render text.picture of card when clicked from controller
