95facbb3b3cdcb6e88563e0a6b2c39f30a88f16d
Alice Fung <alice.o.fung@gmail.com>
2015-08-27 13:22:40 -0400

    flipsquare method

--- card_matcher_mvc.js
+++ card_matcher_mvc.js
@@ -1,10 +1,3 @@
-// Model
-// Check card match
-// Keep game score
-//   - related to board size
-// Card status (flipped)
-// Flipped card count
-// Suggestion: each card has a value
 
 // View
 // Listener on click
@@ -29,44 +22,94 @@
 // Match cards on value, create board with 2 cards of value 1...
 
 
-// 'use strict';
+'use strict';
+
+//shuffle function
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+// Model
+// create cards obj
+// Suggestion: each card has a value
+// Flipped card count
+// Check card match
+// Keep game score
+//   - related to board size
+// Card status (flipped)
 
 var model = {
 
-totalCards: "",
+  totalCards: 0,
 
-cards = {
-  index: value;
-},
+  cards: {},
 
-cardValue: function(card_num){
-  cardValues = [];
-  for(var i = 1; i <= totalCards; i++) {
+  cardValue: function(card_num){
+    var cardValues = [];
+    //create val
+    for(var i = 1; i <= this.totalCards/2; i++) {
+      cardValues.push(i);
+      cardValues.push(i);
+    }
 
-  }
+    //shuffle
+    cardValues = shuffle(cardValues);
 
-  for (var key = 1; key <= totalCards; key++) {
-    cards.key = cardValues.shift();
-  }
-}
+    //assign
+    for (var key = 1; key <= this.totalCards; key++) {
+      this.cards.key = cardValues.pop();
+    }
+  },
+
+  flippedCardCount: 0
 
 
 };
 
 var view = {
   init: function(size){
-    this.renderGameboard(size);
-
+    this.setupGameboard(size);
+    $('#gameboard').click('.hidden-square', function(event){this.flipSquare(event);});
     // this.renderScore();
   },
 
+  render: function(){
+    this.renderScore();
+    // this.renderGameboard();
+  }
   // show score
   renderScore: function(){
 
   },
 
+  flipSquare: function(event){
+    var selectedCard = event.target
+    selectedCard.addClass("revealed-square");
+    selectedCard.removeClass("hidden-square");
+  }
+
+  // renderGameboard: function(){
+  //   //display flip squares
+  //   // $(".revealed-square")
+  // },
+
   // show gameboard
-  renderGameboard: function(size){
+  setupGameboard: function(size){
     console.log(size);
     for( var i=1; i <= size; i++){
       var $card = $('<div class="hidden-square"></div>').text(model.card_value(i));
@@ -82,9 +125,21 @@ var controller = {
     model.totalCards = boardsize;
   },
 
+  flipCard: function(){
+    model.flipCard();
+    view.flipCard();
+    view.render();
+  },
 
   boardsize: function(){
-    return prompt('What is the size of the board?');
+    var userBoardSize = prompt('What is the size of the board?');
+    userBoardSize = Math.floor(userBoardSize);
+    if (userBoardSize %2 !== 0 && userBoardSize !== 0){
+      userBoardSize +=1;
+    }else {
+      userBoardSize = 10;
+    }
+    return userBoardSize;
   }
 
 };
