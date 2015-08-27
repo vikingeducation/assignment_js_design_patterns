cd35b511e001719b1f5c0a570d6c4403c8b594d9
alokp <alokpra@gmail.com>
2015-08-27 08:55:22 -0700

    architecture thought out

---
+++ card_matcher_mvc.js
@@ -0,0 +1,37 @@
+// Model
+// Check card match
+// Keep game score
+//   - related to board size
+// Card status (flipped)
+// Flipped card count
+
+// View
+// Listener on click
+// Create cards
+// Flip card
+// Hide card
+// Generate game / cards
+// Render board
+
+// Controller
+// User prompt to generate game / cards
+//   - Set game score
+//   - Render board
+// User clicks on card
+//   - Flip card
+//   - Check card match if flipped card count = 2
+//   - Hide card if incorrect
+//   - Update score
+//   - Check game end
+// Game end
+
+// Match cards on value, create board with 2 cards of value 1...
+
+
+// 'use strict'
+
+
+
+$(document).ready(function(){
+  controller.init();
+});
