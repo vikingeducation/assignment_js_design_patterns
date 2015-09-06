a996af20837be29eaa0ffb694832821cf8a8ad9b
Adam <adam.kinson@gmail.com>
2015-09-06 17:34:41 -0400

    set up basic model cards

---
+++ script.js
@@ -0,0 +1,68 @@
+'use strict;'
+
+
+var model = {
+  totalCards: 0,
+  cards: [],
+  values: [],
+
+  init: function(numberOfCards) {
+    for(var i = 0; i < numberOfCards; i++) {
+      model.createCard();
+    };
+    model.totalCards = numberOfCards;
+  },
+
+
+  createCard: function() {
+    var newCard = {
+      id: model.cards.length,
+      value: model.generateValue(),
+    };
+
+    model.cards[newCard.id] = newCard;
+  },
+
+  generateValue: function() {
+    var lastValue = model.values[model.values.length - 1];
+    if (model.values.length === 0) {
+      model.values[0] = 'A';
+      return model.values[0];
+    }
+    else if (model.cards.length % 2 === 0) {
+      var newValue = String.fromCharCode(lastValue.charCodeAt(0) + 1);
+      model.values[model.values.length] = newValue;
+      return newValue;
+    }
+    else {
+      return lastValue;
+    }
+  },
+
+  getCard: function() {
+
+  }
+}
+
+
+var view = {
+  init: function() {
+    model.init(prompt('Enter the total number of cards:'));
+  }
+
+    //$newCard = $("<div class='card'></div>");
+    //$('.board').append($newCard);
+}
+
+
+var controller = {
+  init: function() {
+    view.init();
+  }
+}
+
+
+
+$( document ).ready( function() {
+  controller.init();
+})
