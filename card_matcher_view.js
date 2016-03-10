113b1c66078e9609bcef7b360d4accccfbd80c0f
Joshua Masland-Sarani <j.masland@gmail.com>
2016-03-10 12:42:47 -0500

    card event

--- card_matcher_view.js
+++ card_matcher_view.js
@@ -3,6 +3,12 @@ var view = {
   init: function() {
     this.askUser();
     this.addCards();
+
+    $(".card-col").on("click", ".card", function( event ) {
+      cardID = event.target.id;
+      console.log( cardID + " clicked!" );
+      $("#" + cardID).toggleClass("card-revealed");
+    });
   },
 
   askUser: function() {
@@ -12,10 +18,8 @@ var view = {
   addCards: function(){
 
     for (var i = 0; i < (this.numPairs*2); i++ ){
-      var card = $('<div class = "col-sm-3 card-col" ><div class = "card card-'+ i +'"></div></div>');
+      var card = $('<div class = "col-sm-3 card-col" ><div class = "card" id="card-'+ i +'"></div></div>');
       $('#cardholder').append(card);
     }
   },
-
-
 }
