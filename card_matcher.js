7d9807dfc6f7c99362680f71ba5073842e653dde
Kit Langton <kitlangton@gmail.com>
2016-03-10 12:12:48 -0500

    Add functions to card

--- card_matcher.js
+++ card_matcher.js
@@ -1,26 +1,58 @@
-
-
-
-var keys = { 
-
-};
-
-
 var matcherModel = {
-  numPairs: 2,
-  cards: ["A", "B", "A", "B"],
+  size: 4,
+  cards: [],
+  possibleValues: ["A", "B", "C", "D", "E", "F", "G"],
+
+  init: function() {
+    var totalPairs = Math.pow(this.size, 2) / 2;
+    for (var i = 0; i < totalPairs; ++i) {
+      this.addPair();
+    }
+  },
+
+  getCard: function(id) {
+    var card;
+    for (var i = 0; i < this.cards.length; ++i) {
+      card = this.cards[i];
+      if (card.id == id) {
+        return card;
+      }
+    }
+    return false;
+  },
 
   Card: function(id, value) {
     this.id = id;
     this.value = value;
+
+    this.matches = function(otherCard) {
+      if (this.value == otherCard.value) {
+        return true;
+      } else {
+        return false;
+      }
+    }
   },
 
-  addPair : function() {
-    var firstCard = new this.Card();  //need params here
-    var secondCard = new this.Card();
+  addPair: function() {
+    var value = this.randomValue();
+    var firstCard = new this.Card( this.getId(), value);
+    var secondCard = new this.Card( this.getId(), value);
+    this.cards.push(firstCard);
+    this.cards.push(secondCard);
   },
 
-  currentId: 1;
+  randomValue: function() {
+    return this.possibleValues[Math.floor(Math.random() * this.possibleValues.length)];
+  },
+
+  currentId: 1,
+
+  getId: function() {
+    var id = this.currentId;
+    this.currentId += 1;
+    return id;
+  },
 
 };
 
@@ -41,4 +73,3 @@ var matcherView = {
   addCardsToGrid: function() {},
 
 };
-
