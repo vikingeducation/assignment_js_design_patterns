f3453c45df2337bfb0896f4395ab21fe44421d77
Sam O'Keefe <samok13@gmail.com>
2016-03-10 10:28:37 -0800

    more views

--- card_matcher_view.js
+++ card_matcher_view.js
@@ -1,13 +1,49 @@
 var view = {
 
+    revealed: false,
+    card_array: [],
+
   init: function() {
     this.askUser();
     this.addCards();
 
     $(".card-col").on("click", ".card", function( event ) {
-      cardID = event.target.id;
+      var cardID = event.target.id;
       console.log( cardID + " clicked!" );
       $("#" + cardID).toggleClass("card-revealed");
+
+      
+    if (event.target.className.match(/card-revealed/){
+      this.card_array.push(cardID);
+
+      if(this.revealed){
+        if(//compare actual elements of the array ){
+          //solved!
+        }
+        else{
+          this.revealed = false;
+          setTimeout(remove_revealed,1000);
+        }
+      }
+      else{
+        this.revealed = true;
+      }
+
+    }
+
+      //if recent click has card-revealed class
+        //add to card array
+        //check if revealed boolean is true
+          //if element 0, element 1 card array equal?
+            //if eq-win, add a class solved
+            //else revealed = false on both
+            //remove revealed class on both, one sec delay
+          //else
+            //revealed = true
+
+        
+
+
     });
   },
 
