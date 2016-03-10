3faab3761e22a157147367f0a1092210fbf631ec
Sam O'Keefe <samok13@gmail.com>
2016-03-10 09:24:25 -0800

    creating cards

--- card_matcher_view.js
+++ card_matcher_view.js
@@ -2,11 +2,20 @@ var view = {
 
   init: function() {
     this.askUser();
+    this.addCards();
   },
 
   askUser: function() {
     this.numPairs = prompt( "How many pairs do you want to play with?", 2 );
-  }
+  },
+
+  addCards: function(){
+
+    for (var i = 0; i < (this.numPairs*2); i++ ){
+      var card = $('<div class = "col-sm-3 card-col" ><div class = "card card-'+ i +'"></div></div>');
+      $('#cardholder').append(card);
+    }
+  },
 
 
 }
