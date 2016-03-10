ae7111982476a6fdc945e9804555e010356e00ff
Jeffrey Gisin <jgisin@gmail.com>
2016-03-10 11:02:21 -0700

    progress

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-var view = function() {
+var view = {
   // flipping cards
 
   // rowNames = ["one", "two", "three", "four", "five", "six", "seven", "eight"]
-  rowNames = ["one", "two"];
+  rowNames: ["one", "two"],
 
-  placeCards = function(){
+  getRandomRow: function(){
+    return Math.floor(Math.random() * this.rowNames.length)
+  },
 
+
+  renderCard: function(){
+    var randRow = "#" + this.rowNames[this.getRandomRow()];
+    console.log(randRow);
+    console.log("<img src='assets/" + controller.getRandomCard() + "'>")
+    $(randRow).append($("<img src='assets/" + controller.getRandomCard() + "'>"))
   }
-
-}
+};
 
 
 var model = {
@@ -17,21 +24,22 @@ var model = {
   emojis: ["andrew.svg",  "bear.svg",  "bob.svg",
     "canoe.svg",  "cheeky.svg",  "controller.svg",
     "devil.svg",  "eh.svg",  "einstein.svg", "whale.svg",
-    "fithub.svg",  "ghost.svg",  "irs.svg",  "kit.svg",  
+    "fithub.svg",  "ghost.svg",  "irs.svg",  "kit.svg",
     "mtn.svg",  "panda.svg",  "peace.svg",  "plane.svg",
     "poodle.svg",  "pug.svg",  "rainbow.svg",  "ram.svg",
     "rbf.svg",  "robot.svg",  "ruby.svg",  "russia.svg",
     "santa.svg",  "swirlie.svg",  "thumbsup.svg",
     "titanic.svg",  "twitter.svg",  "umbrella.svg"],
 
-  gamePieces: this.emojis,
+  gamePieces: [],
 
   createCards: function() {
     var numRows = 2;
     var uniqueCards = (numRows*numRows) / 2;
     for (var i=0; i < uniqueCards; i++) {
-
+      this.gamePieces.push(this.emojis[ Math.floor(Math.random() * this.emojis.length) ])
     }
+    this.gamePieces = this.gamePieces.concat(this.gamePieces)//awesome!
   },
 
 
@@ -42,7 +50,17 @@ var model = {
 
 
 
-var controller = function() {
+var controller = {
+  init: model.createCards(),
+
+  placeCards: function(){
+    for (var i=0; i < model.gamePieces.length; i++){
+      this.getRandomCard()
+    }
+  },
+  getRandomCard: function(){
+    return model.gamePieces.pop()
+  },
 
 }
 
@@ -50,9 +68,8 @@ var controller = function() {
 $(document).ready(function(){
   // var ourModel = model;
 
-  $('.img-bucket').text(model.emojis)
+  // $('.img-bucket').text(model.emojis)
 
 
 
 });
-
