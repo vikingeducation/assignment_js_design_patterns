2ce7110da91721f2546638a9b87905884eac3c97
Kelsey James <kelseybjames@gmail.com>
2016-03-10 09:36:47 -0800

    reworking card object

--- card_matcher.js
+++ card_matcher.js
@@ -1,27 +1,67 @@
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i -= 1) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+}
 
 var model = {
 
     gridSize: 2,
+    cardList: [],
+    card: {
+      flipped: false,
+      value: ''
+    },
 
     validateGridSize: function(gridSize) {
       if ((gridSize % 2 !== 0) || (gridSize > 8)) {
         return false
       }
       return true
-    }
+    },
 
     getgridSize: function(){
         return this.gridSize;
     },
 
+    generateCards: function() {
+      for (var i=0; i < (Math.pow(gridSize, 2) / 2); i++) {
+        this.card.value = 'foo' + i;
+        this.cardList.push(this.card);
+        this.cardList.push(this.card);
+      };
+      shuffle(this.cardList);
+    },
 
+    getGenerateCards: function() {
+      return this.cardList;
+    },
+
+    getCardValue: function(ourCard) {
+      return ourCard.value;
+    },
+
+    getCardState: function(ourCard) {
+      return ourCard.flipped;
+    },
 };
 
 var controller = {
 
     init: function(){
         view.init();
+        model.generateCards();
+        view.render();
     },
+
+    cardFlip: function(){
+      view.clickCard();
+      model.checkPreviousCard();
+    }
 };
 
 var view = {
@@ -33,6 +73,22 @@ var view = {
        } while(!(model.validateGridSize(gridSize)));
 
     },
+
+    render: function(){
+      cardList = model.getGenerateCards();
+      for(i=0; i < cardList.length; i++) {
+        currentCard = model.cardList[i];
+        console.log(currentCard);
+        newCard = '<div class="card" data-card-value=' + String(model.getCardValue(currentCard)) + '>Card</div>'
+        $('.card-field').append(newCard)
+      }
+    },
+
+    clickCard: function() {
+      $('.card').click(function() {
+        model.getCardState;
+      })
+    }
 
 };
 
