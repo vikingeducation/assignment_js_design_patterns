55847630144a400397c4a501f2eae0a6caa56785
Sia <siakaramalegos@gmail.com>
2016-05-08 14:08:42 -0500

    Basic matching and scores working

--- scripts/controller.js
+++ scripts/controller.js
@@ -5,12 +5,46 @@ var controller = {
   // TODO: change to user-input game size
     model.init(4);
     view.init();
+    this.firstCard = null;
   },
 
-  showCard: function(){
+  showCard: function(event){
+    // Show the card
+    view.showCard($(event.target));
+
+    if (controller.firstCard === null) {
+      // If this is the first card flipped
+      console.log('first flipped');
+      controller.firstCard = $(event.target).attr('src');
+    } else {
+      // If this is the second card flipped
+      console.log('second flipped');
+      // Check match
+      var secondCard = $(event.target).attr('src');
+
+      if (controller.firstCard === secondCard){
+        console.log('match!');
+        model.currentScore += 5;
+        $('.in-play')
+            .removeClass('hidden')
+            .removeClass('in-play');
+            // add class for green
+            // modify model
+      } else {
+        // Hide cards after a 2-second delay
+        window.setTimeout(view.hideCards, 2000);
+
+        // TODO: prevent clicks during delay
+      }
+
+      // Reset first card
+      controller.firstCard = null;
+    }
+
     // model.showCard()
-    // view.render()
+    view.render()
   },
+
 
   getScore: function(){
     return model.getCurrentScore();
--- scripts/view.js
+++ scripts/view.js
@@ -3,6 +3,7 @@
 var view = {
   init: function(){
     // set listeners
+    $('#board').click('.hidden', controller.showCard);
 
     this.render();
     this.renderCards();
@@ -23,5 +24,20 @@ var view = {
 
       $('#board').append($card);
     });
+  },
+
+  showCard: function($target){
+    var actualCard = model.deck[$target.data('id')].card;
+
+    $target
+      .attr('src', 'major_arcana/' + actualCard + '.jpg')
+      .addClass('in-play');
+  },
+
+  hideCards: function(){
+    $('.in-play')
+      .delay(2000)
+      .removeClass('in-play')
+      .attr('src', "major_arcana/back.jpeg");
   }
 };
