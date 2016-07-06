ae9759f1d0c0c5af624c6d9785b947be60217cee
Steven Chang <prime_pork@hotmail.com>
2016-07-06 10:59:46 +1000

    page prompts user for a grid side length

--- card_matcher.js
+++ card_matcher.js
@@ -1,1 +1,22 @@
 'use_strict;'
+
+var controller = {
+	init: function(){
+
+		// Prompting user for a grid side length
+		model.squaresPerSide = Number(prompt("How many squares per side? e.g. '3' for a 3 x 3 grid.", "6"));
+
+	}
+};
+
+var model = {
+	squaresPerSide: 6
+};
+
+var view = {
+
+};
+
+$(document).ready(function(){
+	controller.init();
+});
