8b0c8bcc204742725aab3b5d8bc7c3c7473c9288
Morgan Martin <morganm5201@gmail.com>
2016-09-09 17:59:57 -0400

    card flippin

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var memoryView = {
     for (var i = 0; i < gameCards.length; i++) {
       this.createImgDiv(gameCards[i]);
     }
+    this.cardFlipListener();
   },
 
   createImgDiv: function(gameCard) {
@@ -17,6 +18,7 @@ var memoryView = {
       div.html(this.createImgStr(gameCard.path));
     } else {
       div.html(this.createImgStr('red_joker.png'));
+      div.children().addClass('un-flipped');
     }
     div.attr('id', gameCard.id);
     $('body').append(div);
@@ -29,9 +31,9 @@ var memoryView = {
   },
 
   cardFlipListener: function() {
-    $('img').click(function(e) {
+    $('.un-flipped').click(function(e) {
       $target = $(e.target);
-      var targetId = $target.parent().attr('id')
+      var targetId = $target.parent().attr('id');
       memoryView.callbacks.flipCard(targetId);
     });
   }
@@ -40,6 +42,7 @@ var memoryView = {
 
 var memoryModel = {
   gameCards: [],
+  currentCompCards: [],
   cardNums: ['2','3','4','5','6','7','8','9','10','ace','jack','queen','king'],
   cardSuits: ['spades', 'hearts', 'diamonds', 'clubs'],
   randNum: function(num) { return Math.floor(Math.random() * num); },
@@ -47,10 +50,31 @@ var memoryModel = {
     return this.cardNums[this.randNum(13)] + '_of_' + this.cardSuits[this.randNum(4)] + '.png';
   },
 
+  shuffle: function(array) {
+    var counter = array.length;
+
+    // While there are elements in the array
+    while (counter > 0) {
+      // Pick a random index
+      var index = Math.floor(Math.random() * counter);
+
+      // Decrease counter by 1
+      counter--;
+
+      // And swap the last element with it
+      var temp = array[counter];
+      array[counter] = array[index];
+      array[index] = temp;
+    }
+
+    return array;
+  },
+
   card: function(path, id) {
     this.path = path;
     this.id = id;
     this.flipped = false;
+    this.matched = false;
     return this;
   },
 
@@ -60,24 +84,51 @@ var memoryModel = {
     for(var i = 0; i < numCards; i++) {
       var cardPath = that.randCardPath();
       var firstCard = new that.card(cardPath, i);
-      console.log(firstCard);
       var secondCard = new that.card(cardPath, (i + numCards));
       that.gameCards.push(firstCard);
       that.gameCards.push(secondCard);
     }
-    return this.gameCards;
+    return this.shuffle(this.gameCards);
   },
 
   flipCard: function(targetId) {
     var targetCard = this.findCardById(targetId);
+    var delay;
     targetCard.flipped = true;
-    return this.gameCards;
+    this.currentCompCards.push(targetCard);
+    if (this.currentCompCards.length === 2) {
+      delay = memoryModel.compareCards();
+    }
+    return [this.gameCards, delay];
+  },
+
+  flipCompCards: function() {
+    $.each(this.currentCompCards, function(index, element) {
+      console.log(element);
+      element.flipped = false;
+    });
+    console.log(this.gameCards);
+    this.currentCompCards = [];
+  },
+
+  compareCards: function() {
+    var delay = false;
+    if (this.currentCompCards[0].path === this.currentCompCards[1].path) {
+      console.log('match');
+      $.each(this.currentCompCards, function(index, element) {
+        element.matched = true;
+      });
+      this.currentCompCards = [];
+    } else {
+      console.log('nah');
+      delay = true;
+    }
+    return delay;
   },
 
   findCardById: function(targetId) {
     var gameCards = this.gameCards;
     for (var i = 0; i < gameCards.length; i++) {
-        console.log(gameCards[i])
       if (targetId == gameCards[i].id) {
         return gameCards[i];
       }
@@ -94,7 +145,6 @@ var memoryController = {
     this.view.init({
       flipCard: memoryController.flipCard
       });
-    this.view.cardFlipListener();
   },
 
   setupGrid: function() {
@@ -106,8 +156,19 @@ var memoryController = {
 
   flipCard: function(targetId) {
     var gameCards = memoryController.model.flipCard(targetId);
-    memoryController.view.renderGrid(gameCards);
-    memoryController.view.cardFlipListener();
+    memoryController.view.renderGrid(gameCards[0]);
+    console.log(gameCards[1]);
+    if (gameCards[1]) {
+      memoryController.delayedRender();
+    }
+  },
+
+  delayedRender: function() {
+    memoryController.model.flipCompCards();
+    var gameCards = memoryController.model.gameCards;
+    setTimeout(function() {
+      memoryController.view.renderGrid(gameCards);
+    }, 2000);
   }
 
 };
