c75e9c27795e77b87d5d75bd6b7342505ddd9935
Leo Ahnn <leo.ahnn@gmail.com>
2016-09-09 13:37:49 -0400

    working in integrating controller

--- script.js
+++ script.js
@@ -1,38 +1,78 @@
 $(document).ready(function() {
-  view.init();
+  controller.init();
 })
-
 
 var view = {
 
-  boardWidth: "960px",
+  boardWidth: 960,
 
-  init: function() {
-    var dimensions = prompt("What size grid do you want?");
+  init: function(dimensions) {
+    this.renderSquares(dimensions);
+    this.setEventListeners();
+  },
 
-    for(var i = 0; i < dimensions; i++) {
+  renderSquares: function(dimensions) {
+    for (var i = 0; i < dimensions; i++) {
       var $cardRow = $("<div></div>");
       $cardRow.addClass("row");
       $("#board").append($cardRow);
 
-      for(var j = 0; j < dimensions; j++) {
+      for (var j = 0; j < dimensions; j++) {
         var $cardSpace = $("<div></div>");
+        var sizing = (view.boardWidth / (parseInt(dimensions) + 1));
+        $cardSpace.addClass("card");
         $cardSpace.attr("id", "card" + String(j) + String(i));
-        $cardSpace.width(view.boardWidth/(dimensions + 1));
+        $cardSpace.width(sizing).height(sizing);
         $cardSpace.appendTo($cardRow);
       };
-
-      
     };
+  },
 
+  setEventListeners: function() {
+
+    $('#board').on('click', '.card', function() {
+      controller.flipSquare($(this));
+    });
+  },
+
+  flipSquare: function($square) {
+    $square.css('background-image', 'url(images/' + '' + '.png)');
+    // controller call
   }
 
 };
 
+
+function Square(id) {
+  this.id = id;
+};
+
+
 var model = {
+
+  score: 0,
+
 
 };
 
 var controller = {
+
+  init: function(){
+    var dimensions = prompt("What size grid do you want?");
+    view.init(dimensions);
+    model.createSquares(dimensions);
+  },
+
+  flipSquare: function(){
+    model.flipSquare();
+    view.flipSquare();
+  },
+
+  gameOver: function(){
+    var remaining = model.countRemainingCards();
+    if (!remaining) {
+      return true;
+    }
+  }
 
 };
