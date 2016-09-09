2a02643bb5be635fbec96a70e7d0ae1294fc1e39
“Matthew <matthew.hinea@gmail.com>
2016-09-09 11:16:26 -0700

    add model representation of board and spaces

--- script.js
+++ script.js
@@ -43,8 +43,11 @@ var view = {
 };
 
 
-function Square(id) {
+function Square(id, coords) {
   this.id = id;
+  this.x = coords[0];
+  this.y = coords[1];
+  this.flipped = false;
 };
 
 
@@ -52,15 +55,57 @@ var model = {
 
   score: 0,
 
+  createSquares: function() {
+    for(var i = 0; i < board.dimensions; i++) {
+      for(var j = 0; j < board.dimensions; j++) {
+        var squareCoords = this.setXY();
+        var newSquare = new Square(String(i) + String(j), squareCoords);
+        delete board.spaces[squareCoords];
+      }
+    }
+  },
+
+  setXY: function() {
+    var result;
+    var count = 0;
+    for (var coordinatePair in board.spaces) {
+      if (Math.random() < 1/++count) {
+        result = coordinatePair;
+      }
+    };
+    return result;
+  },
+
+  flipSquare: function(square) {
+
+  }
+
 
 };
+
+var board = {
+
+  spaces: {},
+
+  init: function() {
+    for(var i = 0; i < board.dimensions; i++) {
+      for(var j = 0; j < board.dimensions; j++) {
+        this.spaces[String(i) + String(j)] = true;
+      }
+    }
+  }
+}
 
 var controller = {
 
   init: function(){
     var dimensions = prompt("What size grid do you want?");
-    view.init(dimensions);
+
+    board.dimensions = dimensions;
+    board.init();
+
     model.createSquares(dimensions);
+    view.init(dimensions);
   },
 
   flipSquare: function(){
