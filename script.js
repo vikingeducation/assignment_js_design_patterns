6ae0b21bb95978a110a43d6cadf99b247bc22145
Alex Lach <alexglach@gmail.com>
2016-09-09 13:08:27 -0400

    rendering cards

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 
 memoryView = {
 
+  createImgDiv: function(gameCard) {
+    $('body').append($('<div></div>')
+             .html(this.createImgTag(gameCard)));
+  },
+
+  createImgTag: function(path) {
+    var imgTag = $('<img>');
+    var fullPath = 'PNG-cards-1.3/' + path;
+    imgTag.attr('src', fullPath);
+    return imgTag;
+  },
+
+  renderGrid: function(gameCards) {
+    for (var i = 0; i < gameCards.length; i++) {
+      this.createImgDiv(gameCards[i]);
+    }
+  }
+
 };
 
 memoryModel = {
-  cards: [],
+  gameCards: [],
   cardNums: ['2','3','4','5','6','7','8','9','10','ace','jack','queen','king'],
   cardSuits: ['spades', 'hearts', 'diamonds', 'clubs'],
   randNum: function(num) { return Math.floor(Math.random() * num); },
@@ -12,22 +30,30 @@ memoryModel = {
     return this.cardNums[this.randNum(13)] + '_of_' + this.cardSuits[this.randNum(4)] + '.png';
   },
   createCards: function(numCards) {
-    for(var i = 0; i < numCards - 1; i++) {
-
+    var that = this;
+    for(var i = 0; i < numCards; i++) {
+      var cardPath = that.randCardPath();
+      that.gameCards.push(cardPath)
+      that.gameCards.push(cardPath)
     }
+    return this.gameCards;
   }
 };
 
 var memoryController = {
 
   init: function() {
-    var model = memoryModel;
-    var view = memoryView;
-    console.log(model.randCardPath());
+    this.model = memoryModel;
+    this.view = memoryView;
+    this.setupGrid();
   },
 
   setupGrid: function() {
     var gridSize = prompt('How many pairs to play with? (even)');
+    var gameCards = this.model.createCards(gridSize);
+    console.log(gameCards);
+    this.view.renderGrid(gameCards);
+
   },
 
 };
