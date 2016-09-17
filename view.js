6e868ea7285c382749194cb76fe8bbd4cc476958
BranLiang <lby89757@hotmail.com>
2016-09-17 17:40:42 +0800

    setup pictures

--- view.js
+++ view.js
@@ -13,14 +13,25 @@ var view = {
 		});
 	},
 
+
+
 	setUpGrids: function () {
 		var size = controller.readGridValue();
 		var $block = $("<div></div>").addClass("img-block");
 		var picArray = controller.readPicArray();
+		var $bgPicture = $("<img>").attr("src", "img/bg.jpg");
+
 		for (var i = 0; i < size; i++) {
 			var picIndex = picArray.pop();
 			var $randomPicture = $("<img>").attr("src", "img/" + picIndex + ".jpg")
-			$('main section').append($block.clone().append($randomPicture));
+				.addClass("back");
+			var $front = $("<div></div>").addClass("front")
+				.append($bgPicture.clone());
+			var $back = $("<div></div>").addClass("back")
+				.append($randomPicture);
+			$('main section').append($block.clone()
+				.append($front)
+				.append($back));
 		};
 	},
 
