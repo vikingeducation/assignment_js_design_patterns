e1394181747fb8da3680b1247f0264d85331300c
Deepak <grc.deepak@gmail.com>
2016-11-11 23:31:01 -0800

    game done

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var model = {
   gridSize: 0,
   images: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17'],
   imageUrls: [],
+  temporalList: [],
+  score: 0,
   init: function() {
     var numUrls = model.gridSize * model.gridSize;
     for (var i = 0; i < numUrls/2; i++) {
@@ -9,7 +11,16 @@ var model = {
       model.imageUrls.push('images/' + randomImage + '.png');
       model.imageUrls.push('images/' + randomImage + '.png');
     }
+  },
+
+  incrScore: function() {
+    model.score += 1;
+  },
+
+  decrScore: function() {
+    model.score -= 1;
   }
+
 
 }
 
@@ -42,9 +53,22 @@ var view = {
     }
   },
 
+  OnBackOfTheCardListener: function() {
+    $('img.backOfTheCard').on('click', function() {
+      controller.flip(this);
+      view.markAsTemp(this);
+      controller.isMatch(this);
+    });
+  },
+
+  OffBackOfTheCardListener: function() {
+    $('img.backOfTheCard').off('click')
+  },
+
   init: function() {
     var ret = false;
     var gridSize = prompt("Please enter the grid size (Even Numbers only)");
+    // var gridSize = 4;
     if (gridSize != null && gridSize % 2 == 0) {
       model.gridSize = Number(gridSize);
       ret = true;
@@ -53,18 +77,42 @@ var view = {
     //setup
     this.gridSetup();
 
-    $('.col').on('click', 'img', function() {
-      controller.flip(this);
-    });
+    view.OnBackOfTheCardListener();
 
+    $('.start-game').on('click', function() {
+      location.reload();
+    });
 
     return ret;
   },
+
+  clear: function() {
+    var first_row_col = $('.container .row .col').first().clone();
+    $('.container .row').remove();
+    first_row_col.appendTo($('.container'));
+  },
+
+  renderScore(newScore) {
+    $('span.score').html(newScore)
+  },
+
+  markAsOpened(elem) {
+    $(elem).parent().children().removeClass('closed temp').addClass('opened');
+  },
+
+  markAsTemp(elem) {
+    $(elem).parent().children().removeClass('closed opened').addClass('temp');
+  },
+
+  markAsClosed(elem) {
+    $(elem).parent().children().removeClass('temp opened').addClass('closed');
+  }
 
 };
 
 var controller = {
   init: function() {
+
     while(true) {
       if (view.init() == true) {
         break;
@@ -74,6 +122,33 @@ var controller = {
 
   flip: function(elem) {
     $(elem).parent().children().toggleClass('facedown').toggleClass('faceup');
+  },
+
+  isMatch: function(elem) {
+    var newImgObj =  $(elem).siblings().first();
+    if (model.temporalList.length === 0) {
+      model.temporalList.push(newImgObj);
+      return;
+    }
+
+    view.OffBackOfTheCardListener();
+    var listImgObj = model.temporalList.pop();
+    if (newImgObj.attr('src') === listImgObj.attr('src')) {
+      model.incrScore();
+      view.markAsOpened(newImgObj);
+      view.markAsOpened(listImgObj);
+      view.renderScore(model.score);
+      view.OnBackOfTheCardListener();
+    } else {
+      model.decrScore();
+      setTimeout( function() {
+        controller.flip($('.temp'));
+        view.markAsClosed(newImgObj);
+        view.markAsClosed(listImgObj);
+        view.OnBackOfTheCardListener();
+      }, 2000);
+      view.renderScore(model.score);
+    }
   }
 }
 
