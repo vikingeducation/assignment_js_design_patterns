e6e5cc66ac2c5e55eafa1127e3314ea7da6ff416
test <kennykotten@gmail.com>
2017-01-31 03:13:12 +0000

    added shuffling functionality

--- model.js
+++ model.js
@@ -9,12 +9,22 @@ var model = {
 
   getGridSize: function(){
     return this.gridSize;
+  },
+
+  createPictureCache: function(pictureQuantity){
+    var newPicture,
+        pictureCache = [],
+        i = (pictureQuantity / 2);
+    
+    for (i; i > 0; i--) {
+      newPicture = $("<div>", {class: "picture"});
+      newPicture.identifier = String(i);
+      
+      pictureCache.push(newPicture, newPicture);
+    }
+    
+    return pictureCache.sort(function(a,b){ 
+      return Math.floor(Math.random() * pictureCache.length)
+    });
   }
-  
-  
-
 }; //model end
-
-
-
-// var $div = $("<div>", {id: "foo", "class": "a"});
--- view.js
+++ view.js
@@ -5,32 +5,19 @@ var view = {
 
   // },
 
-  // render: function(){
+  render: function(){
+  },
 
-  // },
+  container: $(".game-wrapper"),
   
-  
-  
-  
-  
-  // prompt user for grid size
+  // prompts user for grid size
   promptUser: function(){
     return Number(prompt("Enter the amount of pictures for board size. (Choose an even number between 4 - 20"));
   },
   
-  container: $(".game-wrapper"),
-
-  createPictures: function(pictureQuantity){
-    var newPicture,
-    i = pictureQuantity / 2;
+  // displayPictures: function(){
     
-    for (i; i > 0; i--) {
-      newPicture = $("<div>", {class: "picture"});
-      // this.container.append()
-      console.log(i)
-      console.log(newPicture)
-      this.container.append(newPicture)
-    }
-  }
-
+  // },
+  
+  
 }; //view end
